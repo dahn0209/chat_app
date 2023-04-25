bfd07611f6efa6fc4554802c0593c856460091e3
dahn0209 <dahn0209@gmail.com>
2023-04-25 04:00:03 -0400

    secret

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 const express=require('express');
-const mongoose=require('mongoose')
+const mongoose=require('mongoose');
+const User=require('./models/User');
+const jwt=require('jsonwebtoken')
 
 const app=express();
 
 const dotenv=require('dotenv')
 
 dotenv.config()
-
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL)////connect to database///
 
 
 app.get('/test',(req,res)=>{
@@ -15,8 +16,13 @@ app.get('/test',(req,res)=>{
 })
 
 
-app.post('/register',(req,res)=>{
-    res.json('test deeee!')
+app.post('/register',async (req,res)=>{
+   const {username,password}=req.body;
+   const createdUser=await User.create({username,password});
+   jwt.sign(
+    {
+        userId:createdUser._id
+   })
 })
 
 app.listen(4000) 
