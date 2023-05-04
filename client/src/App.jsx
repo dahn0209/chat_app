4150f0866cb3b533940bf67f3ee45385c4662120
dahn0209 <dahn0209@gmail.com>
2023-05-04 01:08:00 -0400

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,15 @@
-import Register from "./Register";
 import axios from 'axios'
-import { UserContext, UserContextProvider } from "./UserContext";
-import { useContext } from "react";
+import { UserContextProvider } from "./UserContext";
+import Routes from "./Routes";
 
 
 function App() {
   axios.defaults.baseURL='http://localhost:4000';
   axios.defaults.withCredentials=true;
-  const {username}=useContext(UserContext)
-  console.log('look at username at Apps=>',username)
 
   return (
     <UserContextProvider>
-       <Register/>
+       <Routes/>
     </UserContextProvider>
    
   )
