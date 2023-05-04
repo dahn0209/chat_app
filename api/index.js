e76f1d3b7897316812e300277b1a842b8c007ddb
dahn0209 <dahn0209@gmail.com>
2023-05-04 03:53:02 -0400

    working on profile

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
-const dotenv=require('dotenv')
+const dotenv=require('dotenv');
 const mongoose=require('mongoose');
+const cookieParser=require('cookie-parser');
 const User=require('./models/User');
 const jwt=require('jsonwebtoken');
 const cors=require('cors');
@@ -13,6 +14,7 @@ jwtSecret=process.env.JWT_SECRET;
 
 const app=express();
 app.use(express.json());
+app.use(cookieParser())
 app.use(cors({
     credentials:true,
     origin: process.env.CLIENT_URL,
@@ -24,15 +26,29 @@ app.get('/test',(req,res)=>{
 })
 
 
+app.get('/profile',(req,res)=>{
+    const token=req.cookies?.token;
+    if(token){
+        jwt.vertify(token,jwtSecret,{},(err,userData)=>{
+            if(err) throw err;
+            const {id,username}=userData;
+            res.json(userData);
+        })
+    }else{
+        res.status(401).json('no token')
+    }
+
+})
+
 app.post('/register',async (req,res)=>{
     const {username,password}=req.body;
    try{
     const createdUser=await User.create({username,password});
     jwt.sign({userId:createdUser._id },jwtSecret,{},(err,token)=>{
         if(err) throw err;
-        res.cookie('token',token).status(201).json({
+        res.cookie('token',token, {sameSite:'none',secure:true}).status(201).json({
             id:createdUser._id,
-            // username:createdUser.username
+            username,
             
         })
        });
--- client/src/userContext.jsx
+++ client/src/userContext.jsx
@@ -1,10 +1,17 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
+import axios from "axios"
 
 export const UserContext=createContext({});
 
 export function UserContextProvider({children}){
     const [username,setUsername]=useState(null);
     const [id,setId]=useState(null);
+    useEffect(()=>{
+        axios.get('/profile').then(response=>{
+            console.log('see==>',response.data)
+        })
+
+    }, []);
 
     return (
         <UserContext.Provider value={{username,setUsername,id,setId}}>
