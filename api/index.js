c8a7ad425a9fc8bc386c55dc74fbe3cd3e6ac108
dahn0209 <dahn0209@gmail.com>
2023-05-06 04:51:44 -0400

    fixed login

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ const cookieParser=require('cookie-parser');
 const User=require('./models/User');
 const jwt=require('jsonwebtoken');
 const cors=require('cors');
+const bcrypt=require('bcryptjs');
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL)////connect to database///
 jwtSecret=process.env.JWT_SECRET;
+const bcryptSalt=bcrypt.genSaltSync(10)
 const app=express();
 app.use(express.json());
 app.use(cookieParser())
@@ -37,10 +39,31 @@ app.get('/profile',(req,res)=>{
 
 })
 
+app.post('/login', async(req,res)=>{
+    const {username,password}=req.body;
+    const foundUser=await User.findOne({username});
+    if(foundUser){
+       const passOk= bcrypt.compareSync(password, foundUser.password);
+       if(passOk){
+        jwt.sign({userId:foundUser._id,username}, jwtSecret,{}, (err,token)=>{
+            res.cookie('token',token,{sameSite:'none',secure:true}).json({
+                id:foundUser._id
+            })
+        });
+       }
+    }
+
+
+} )
+
 app.post('/register',async (req,res)=>{
     const {username,password}=req.body;
    try{
-    const createdUser=await User.create({username,password});
+    const hashedPassword=bcrypt.hashSync(password,bcryptSalt)
+    const createdUser=await User.create({
+        username:username,
+        password: hashedPassword
+    });
     jwt.sign({userId:createdUser._id,username},jwtSecret,{},(err,token)=>{
         if(err) throw err;
         res.cookie('token',token, {sameSite:'none',secure:true}).status(201).json({
